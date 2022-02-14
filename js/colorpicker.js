3858c4a4293dcada8b6069ff7bef157939cf5520
Bruce Brown <bbrown32@student.cvtc.edu>
2022-02-14 08:34:59 -0600

    [Delivers #181245277] Added luminance calculation and display.

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -4,36 +4,38 @@ Script is designed to get the RGB data and change
 it to PYBL data. It then will input the data in labels.
 */
 
-    document.querySelector("#RGBForm").addEventListener("submit", function(event) {
-      
-      var red = Number(document.querySelector("#red").value);
-      var green = Number(document.querySelector("#green").value);
-      var blue = Number(document.querySelector("#blue").value);
-      
-      // Validates only numbers between 0 and 1 were inputted
-      if(red == '' || isNaN(red) || green == '' || isNaN(green) || blue == '' || isNaN(blue) ){
-        alert("All fields require a number!");
-      }
-      
-      else if (red < 0 || red > 1 || green < 0 || green > 1 || blue < 0 || blue > 1){
-        alert("Please only select numbers between 0 and 1!");
-      }
-      
-      else {
-        
-        // Calculates what PYBL will be from RGB values submitted
-        var totalRGB = (red + green + blue);
-        var rho = (red / totalRGB).toFixed(2);
-        var gamma = (green / totalRGB).toFixed(2);
-        var beta = (blue / totalRGB).toFixed(2);
-        
-        // Displays the PYBL labels and adds the data
-        document.querySelector("#Conversion").style.display = "block";
-        document.querySelector("#rho").innerHTML += (" " + rho);
-        document.querySelector("#gamma").innerHTML += (" " + gamma);
-        document.querySelector("#beta").innerHTML += (" " + beta);
-      }
-      
-      // Stops the form from getting refreshed by the browser
-      event.preventDefault();
-    }, false)
+document.querySelector("#RGBForm").addEventListener("submit", function(event) {
+  
+  var red = Number(document.querySelector("#red").value);
+  var green = Number(document.querySelector("#green").value);
+  var blue = Number(document.querySelector("#blue").value);
+  
+  // Validates only numbers between 0 and 1 were inputted
+  if(red == '' || isNaN(red) || green == '' || isNaN(green) || blue == '' || isNaN(blue) ){
+    alert("All fields require a number!");
+  }
+  
+  else if (red < 0 || red > 1 || green < 0 || green > 1 || blue < 0 || blue > 1){
+    alert("Please only select numbers between 0 and 1!");
+  }
+  
+  else {
+    
+    // Calculates what PYBL will be from RGB values submitted
+    var totalRGB = (red + green + blue);
+    var rho = (red / totalRGB).toFixed(2);
+    var gamma = (green / totalRGB).toFixed(2);
+    var beta = (blue / totalRGB).toFixed(2);
+    var luminance = Math.max(rho, gamma, beta);
+    
+    // Displays the PYBL labels and adds the data
+    document.querySelector("#Conversion").style.display = "block";
+    document.querySelector("#rho").innerHTML += (" " + rho);
+    document.querySelector("#gamma").innerHTML += (" " + gamma);
+    document.querySelector("#beta").innerHTML += (" " + beta);
+    document.querySelector("#luminance").innerHTML += (" " + luminance);
+  }
+  
+  // Stops the form from getting refreshed by the browser
+  event.preventDefault();
+}, false)
